1796984ffec50a1ac67a1f5c0097a4f7e97688bf
u2 <crash.skofild@gmail.com>
2016-09-15 15:18:19 +0300

    lesson 10

--- public/html/contacts.php
+++ public/html/contacts.php
@@ -5,22 +5,28 @@ include "head.php";
 ?>
 
     <main>
-        <header class="main_header">
-            <h1>Contact Us</h1>
-            <section class="desc">
-                <p>Nam in maximus arcu, ac aliquam tellus. Donec vestibulum ipsum nunc, at placerat ante posuere non. Integer at dui a lacus suscipit elementum id non massa.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc eu neque eros. Ut eu quam justo.</p>
-            </section>
-        </header>
-        <div class="main_text">
-            <form action="" method="post">
-                <input class="inputs" type="text" name="name" placeholder="Your Name" required>
-                <input class="inputs" type="email" name="email" placeholder="Email" required>
-               <p><input class="inputs_sub" type="text" name="subject" placeholder="Subject" required></p>
-               <p><textarea class="text_area" name="message" placeholder="Your message" required></textarea></p>
-                <p><input type="submit" value="SEND MESSAGE" class="sub_button"></p>
+        <div class="container">
+            <article class="row example">
+                <div class="col s-12">
+                    <header class="main_header">
+                        <h1>Contact Us</h1>
+                        <section class="desc">
+                            <p>Nam in maximus arcu, ac aliquam tellus. Donec vestibulum ipsum nunc, at placerat ante posuere non. Integer at dui a lacus suscipit elementum id non massa.
+                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc eu neque eros. Ut eu quam justo.</p>
+                        </section>
+                    </header>
+                     <div class="main_text">
+                <form action="" method="post">
+                    <input class="inputs" type="text" name="name" placeholder="Your Name" required>
+                    <input class="inputs" type="email" name="email" placeholder="Email" required>
+                   <p><input class="inputs_sub" type="text" name="subject" placeholder="Subject" required></p>
+                   <p><textarea class="text_area" name="message" placeholder="Your message" required></textarea></p>
+                    <p><input type="submit" value="SEND MESSAGE" class="sub_button"></p>
 
-            </form>
+                </form>
+            </div>
+                </div>
+            </article>
         </div>
     </main>
 
--- public/html/post.php
+++ public/html/post.php
@@ -4,42 +4,46 @@
 include "head.php";
 ?>
     <main>
-        <header class="art_header">
+        <div class="container">
+            <article class="row example">
+            <div class="col s-12">
+                <header class="art_header">
 
-            <div class="i_do">
-                I Do Reflect
+                    <div class="i_do">
+                        <a href="">I Do Reflect</a>
+                    </div>
+                    <div class="art_date">
+                        Sep 11
+                    </div><br>
+                    <h2>Democracy means simply the bludgeoning of the people by the people for the people. </h2>
+                </header>
+
+                <div class="art_text">
+                    <div class="img_text"> <img src="../img/img_6.jpg" alt="Image 6"></div>
+                    <p>Quisque congue lacus mattis massa luctus, nec hendrerit purus aliquet. Ut ac elementum urna. Pellentesque suscipit metus et egestas congue. Duis eu pellentesque turpis, ut maximus metus.
+                        Sed ultrices tellus vitae rutrum congue. Fusce luctus augue id nisl suscipit, vel sollicitudin orci egestas. Morbi posuere venenatis ipsum, ac vestibulum quam dignissim efficitur.
+                        Ut vitae rutrum augue, in volutpat quam. Cras a viverra ipsum. Aenean ut consequat ex, vitae vulputate nunc. Vestibulum metus nisi, aliquam sed tincidunt sit amet, pretium et augue.</p>
+                    <p>Mauris sem odio, rhoncus eget euismod sed, pellentesque sit amet felis. Praesent dictum eleifend dui et efficitur. Nunc non orci in neque sodales semper.
+                        Etiam euismod volutpat lorem, <a href="">vestibulum malesuada</a> justo aliquet eget. Nunc lacus ante, varius a euismod eu, finibus commodo erat. Curabitur tincidunt sit amet nunc id interdum.
+                        Aliquam augue nisi, venenatis vitae ex at, lobortis fringilla nibh. Proin placerat enim vitae egestas eleifend. Aliquam quis orci dignissim, posuere nibh a, eleifend augue.
+                        Cras quis metus nec tortor aliquet ullamcorper. Quisque varius porta metus, ut maximus dolor euismod nec. Suspendisse sit amet feugiat turpis.</p>
+                    <blockquote><p>Curabitur ut leo pulvinar, consectetur magna eu, feugiat purus. Morbi hendrerit lectus turpis, a porttitor velit imperdiet id. </p></blockquote>
+                    <p>Nunc placerat dolor at lectus hendrerit dignissim. Ut tortor sem, consectetur nec hendrerit ut, ullamcorper ac odio. Donec viverra ligula at quam tincidunt imperdiet. Nulla mattis tincidunt auctor.
+                        Nullam scelerisque ante mauris, egestas commodo nisi gravida ultrices. Suspendisse dapibus feugiat tincidunt.
+                        Aliquam gravida quam at ipsum sagittis bibendum. </p>
+                </div>
+
+                <footer class="art_footer">
+
+                    <div class="tags col xs-12 m-12">
+                        <span class="tag"><a href="">#Democracy</a></span>
+                        <span class="tag"><a href="">#Introspection</a></span>
+                    </div>
+                </footer>
             </div>
-            <div class="art_date">
-                Sep 11
-            </div><br>
-            <h2>Democracy means simply the bludgeoning of the people by the people for the people.</h2>
-        </header>
-        <div class="art_text">
-            <div class="img_text"><img src="../img/img_6.jpg" alt="Image 6"></div>
-            <p> Quisque congue lacus mattis massa luctus, nec hendrerit purus aliquet. Ut ac elementum urna. Pellentesque suscipit metus et egestas congue. Duis eu pellentesque turpis, ut maximus metus.
-                Sed ultrices tellus vitae rutrum congue. Fusce luctus augue id nisl suscipit, vel sollicitudin orci egestas. Morbi posuere venenatis ipsum, ac vestibulum quam dignissim efficitur.
-                Ut vitae rutrum augue, in volutpat quam. Cras a viverra ipsum. Aenean ut consequat ex, vitae vulputate nunc. Vestibulum metus nisi, aliquam sed tincidunt sit amet, pretium et augue.</p>
-            <p>Mauris sem odio, rhoncus eget euismod sed, pellentesque sit amet felis. Praesent dictum eleifend dui et efficitur. Nunc non orci in neque sodales semper.
-                Etiam euismod volutpat lorem, <a href="">vestibulum malesuada</a> justo aliquet eget. Nunc lacus ante, varius a euismod eu, finibus commodo erat. Curabitur tincidunt sit amet nunc id interdum.
-                Aliquam augue nisi, venenatis vitae ex at, lobortis fringilla nibh. Proin placerat enim vitae egestas eleifend. Aliquam quis orci dignissim, posuere nibh a, eleifend augue.
-                Cras quis metus nec tortor aliquet ullamcorper. Quisque varius porta metus, ut maximus dolor euismod nec. Suspendisse sit amet feugiat turpis.</p>
-            <blockquote><p>Curabitur ut leo pulvinar, consectetur magna eu, feugiat purus. Morbi hendrerit lectus turpis, a porttitor velit imperdiet id. </p></blockquote>
-            <p>Nunc placerat dolor at lectus hendrerit dignissim. Ut tortor sem, consectetur nec hendrerit ut, ullamcorper ac odio. Donec viverra ligula at quam tincidunt imperdiet. Nulla mattis tincidunt auctor.
-                Nullam scelerisque ante mauris, egestas commodo nisi gravida ultrices. Suspendisse dapibus feugiat tincidunt.
-                Aliquam gravida quam at ipsum sagittis bibendum. </p>
+
+        </article>
         </div>
-        <footer class="art_footer">
-            <div class="art_social">
-
-            </div>
-            <div class="tags">
-                <a href="">#Democracy</a>
-                <a href="">#Introspection</a>
-            </div>
-            <div class="disqus">
-
-            </div>
-        </footer>
     </main>
 <?php
 include "footer.php";
